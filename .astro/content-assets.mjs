decc0edae1fd68f34df30c6b21cd16247e438bd8
root <root@vps-241ac57e.vps.ovh.ca>
2026-02-18 15:01:11 +0000

    feat: limit homepage to 6 posts + add reading time to cards and posts

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_2bi215 from "./images/git-flow-diagram.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-12-28-our-custom-git-flow.mdx";
+export default new Map([["./images/git-flow-diagram.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-12-28-our-custom-git-flow.mdx", __ASTRO_IMAGE_IMPORT_2bi215]]);
+		
